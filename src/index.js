3b7d1c372a4e84938f41356a7240bd3ff76a8058
Franco Barberis <francobarberissic3@gmail.com>
2026-01-13 11:10:19 -0300

    server essential imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // arranque server HTTP + Socket.IO
+
+import "dotenv/config"; //carga las variables de entorno desde el archivo .env
+import express from "express"; //para crear servidor y definir rutas HTTP
+import http from "http"; //socketIO lo necesita para engancharse al servidor
+import cors from "cors"; //para definir quienes puede consumir mi servidor
+import {Server as SocketIOServer} from "socket.io"; //para crear servidor de sockets
+
